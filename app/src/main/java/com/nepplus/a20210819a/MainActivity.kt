c55008d4c72c90be2e0563375be60bb6d2fd1221
김현지 <guswlhjk@naver.com>
2021-08-22 20:23:28 +0900

    입력한 내용을 받아와서 토스트 띄우기

--- app/src/main/java/com/nepplus/a20210819a/MainActivity.kt
+++ app/src/main/java/com/nepplus/a20210819a/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nepplus.a20210819a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        toastBtn.setOnClickListener { 
+            
+            val inputMessage = messageEdt.text.toString()
+
+            Toast.makeText(this, "inputMessage", Toast.LENGTH_SHORT).show()
+        }
     }
 }
