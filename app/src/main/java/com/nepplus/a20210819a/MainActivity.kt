18abea245bd52429dcd045663eccbacb61c42978
김현지 <guswlhjk@naver.com>
2021-08-22 20:31:08 +0900

    ui코틀린에서 조작해보기

--- app/src/main/java/com/nepplus/a20210819a/MainActivity.kt
+++ app/src/main/java/com/nepplus/a20210819a/MainActivity.kt
@@ -9,6 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        moveToTextBtn.setOnClickListener {
+
+            val inputMessage = messageEdt.text.toString()
+            resultTxt.text = inputMessage
+
+        }
         
         toastBtn.setOnClickListener { 
             
